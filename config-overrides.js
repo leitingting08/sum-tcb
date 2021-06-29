faf3d56743873f6404740de01f6efca12e308564
ltt <leitingting08@gmail.com>
2021-06-29 20:22:53 +0800

    feat: 🎉  init

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
+const { getThemeVariables } = require('antd/dist/theme')
+const path = require('path')
+
+module.exports = override(
+    fixBabelImports(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: getThemeVariables({
+          dark: true, // 开启暗黑模式
+          // compact: true // 开启紧凑模式
+        })
+    }),
+    addWebpackAlias({
+        '@': path.resolve('./src')
+    })
+)
